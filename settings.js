c81771950489cae1febb4372b377723a10887910
thomashoustongdrecruitment-afk <thomas.houston.gd.recruitment@gmail.com>
2025-11-29 16:46:46 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'SAMADHINO', // Your name
+  ownerNumber: '2348130961035', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
